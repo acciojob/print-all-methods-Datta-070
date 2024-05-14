7cb21560c00bcf09f9028b7718f5925a4f01eb7e
Chinmay Datt D <chinmaydatt003@gmail.com>
2024-05-14 11:13:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 //your JS code here. If required.
 function allMethods() {
   //write your code here
+	const mathMethods = Object.getOwnPropertyNames(Math)
+        .filter(method => typeof Math[method] === 'function');
+    return mathMethods.join(', ');
 }
 
 alert(allMethods());
